ce4ee5f0414f3dc564771632838056c953172fe6
Ashok Gelal <ashokgelal@gmail.com>
2014-09-09 21:18:22 -0600

    Mutable functional example.

---
+++ FSharpPlayground/FSharpPlayground/MutableFunction.fs
@@ -0,0 +1,21 @@
+﻿module MutableFunction
+
+type Widget =
+    | Sprocket of string * int
+    | Cog of string * float
+
+
+let mutable generateWidget =
+    let count = ref 0
+    (fun() -> incr count
+              Sprocket("Model A1", !count))
+
+
+generateWidget()
+
+generateWidget <- 
+    let count = ref 0
+    (fun() -> incr count
+              Cog ((sprintf "Version 0x%x" !count), 0.0))
+
+generateWidget()
